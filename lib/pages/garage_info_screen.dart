7916eb7a4f8b907fd36aadba47927f825d795ac6
mauricekorea <markkalyango23@gmail.com>
2024-07-30 11:48:15 +0300

    adding mechanic details form

---
+++ lib/pages/garage_info_screen.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+class GarageInfoScreen extends StatefulWidget {
+  const GarageInfoScreen({super.key});
+
+  @override
+  State<GarageInfoScreen> createState() => _GarageInfoScreenState();
+}
+
+class _GarageInfoScreenState extends State<GarageInfoScreen> {
+  final carModelTextEditingController = TextEditingController();
+  final carNumberTextEditingController = TextEditingController();
+
+  List<String> carTypes = ["Car", "CNG", "bike"];
+  String? selectedCarType;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    bool darkTheme =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },  
+      child: Scaffold(
+        body: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            Column(
+              children: [
+                Image.asset(darkTheme?),
+
+                SizedBox(height: 20,),
+                Text(
+                  "Add Car Details",
+                  style: TextStyle(
+                    color: darkTheme ? Colors.amber.shade400:Colors.blue,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 20, 15, 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Form(
+                        key: _formKey,
+                        mainAxisAlignment :MainAxisAlignment.center,
+                        crossAxisAligment : CrossAxisAlignment.center,
+                        children:[
+                          TextFormField(
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(50)
+                            ],
+                            decoration: InputDecoration(
+                              hintText: "CarModel",
+                              hintStyle: TextStyle(
+                                color:Colors.grey,
+                              ),
+                              filled: true,
+                              fillColor: darkTheme? Colors.black45:Colors.grey.shade200,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                )
+                              ),
+                              prefixIcon: Icon(Icons.person,color: darkTheme? Colors.amber.shade400:Colors.grey,)
+                            ),
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (text){
+                              if(text == null || text.isEmpty){
+                                return 'Name can\'t be empty';
+                              }
+                              if(text.length<2){
+                                return "Please enter a valid name";
+                              }
+
+                            },
+                            onChanged: (text) =>setState(() {
+                              CarModelTextEditingController.text = text;
+                            });,
+                          )
+                          SizedBox(height: 20,),
+
+                          TextFormField(
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(50)
+                            ],
+                            decoration: InputDecoration(
+                              hintText: "Car Number",
+                              hintStyle: TextStyle(
+                                color:Colors.grey,
+                              ),
+                              filled: true,
+                              fillColor: darkTheme? Colors.black45:Colors.grey.shade200,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                )
+                              ),
+                              prefixIcon: Icon(Icons.person,color: darkTheme? Colors.amber.shade400:Colors.grey,)
+                            ),
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (text){
+                              if(text == null || text.isEmpty){
+                                return 'Name can\'t be empty';
+                              }
+                              if(text.length<2){
+                                return "Please enter a valid name";
+                              }
+
+                            },
+                            onChanged: (text) =>setState(() {
+                              CarNumberTextEditingController.text = text;
+                            });,
+                          )
+                          SizedBox(height: 20,),
+
+                          TextFormField(
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(50)
+                            ],
+                            decoration: InputDecoration(
+                              hintText: "Name",
+                              hintStyle: TextStyle(
+                                color:Colors.grey,
+                              ),
+                              filled: true,
+                              fillColor: darkTheme? Colors.black45:Colors.grey.shade200,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                )
+                              ),
+                              prefixIcon: Icon(Icons.person,color: darkTheme? Colors.amber.shade400:Colors.grey,)
+                            ),
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (text){
+                              if(text == null || text.isEmpty){
+                                return 'Name can\'t be empty';
+                              }
+                              if(text.length<2){
+                                return "Please enter a valid name";
+                              }
+
+                            },
+                            onChanged: (text) =>setState(() {
+                              CarModelTextEditingController.text = text;
+                            });,
+                          )
+                          SizedBox(height: 20,),
+
+                          DropdownButtonFormField(
+                            decoration: InputDecoration(
+                              hintText:"Please Choose Car Type",
+                              prefixIcon: Icon(Icons.car_crash,color: darkTheme? Colors.amber.shade400:Colors.grey,),
+                              filled: true,
+                              fillColor: darkTheme? Colors.black45:Colors.grey.shade200,
+                              border:OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                )
+                              )
+                            ),
+                            items: carTypes.map((car){
+                              return DropdownMenuItem(
+                                child: Text(
+                                  car,
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                value: car,
+                              );
+                            }).toList(), 
+                            onChanged: (newValue){
+                              setState(() {
+                                selectedCarType = newValue.toString();
+                              });
+                            })
+                            SizedBox(height: 20,)
+                        ]
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
